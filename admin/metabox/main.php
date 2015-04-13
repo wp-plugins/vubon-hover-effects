4189b4a446e309f8bd26a8f267e0e58ef9edd08f
mr_sagar <mr_sagar@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-13 06:15:37 +0000

    updated main.php
    git-svn-id: https://plugins.svn.wordpress.org/vubon-hover-effects/trunk@1133482 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/metabox/main.php
+++ admin/metabox/main.php
@@ -12,6 +12,14 @@ return array(
 		'title'     => __('Vubon Hover Item', 'vp_textdomain'),
 		'fields'    => array(
 		
+						array(
+			'type' => 'notebox',
+			'name' => 'nb_1',
+			'label' => __('Author Comment', 'vp_textdomain'),
+			'description' => __('If you want to enable awesome features, you need to buy pro version from here <a target="_blank" href="http://wpexpert24.com/vubon-hover-effects-pro/">Vubon Hover Effects Pro</a>', 'vp_textdomain'),
+			'status' => 'error',
+				),
+		
 			 array(
 				'type' => 'upload',
 				'name' => 'imgup_1',
@@ -34,7 +42,13 @@ return array(
 			'default' => 'Enter your Description',
 			),
 			
-			
+			array(
+			'type' => 'textbox',
+			'name' => 'link',
+			'label' => __('Enter Image URL <br/><a href="http://wpexpert24.com/vubon-hover-effects-pro/" style="color: red;">Pro Only</a>', 'vp_textdomain'),
+			//'description' => __('Only **alphabets** allowed here.', 'vp_textdomain'),
+			'default' => '#',
+			),	
 			
 
 		),
